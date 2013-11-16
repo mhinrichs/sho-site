2fb1e4e53afb76c57dede3d470e5532f81bcd6f8
Matthew Hinrichs <mhinrichs@gmail.com>
2013-11-16 20:48:20 +0900

    deleted files related to finished tutorial

--- sho-site/rango/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page, UserProfile
-
-admin.site.register(Category)
-admin.site.register(Page)
-admin.site.register(UserProfile)
-
-
